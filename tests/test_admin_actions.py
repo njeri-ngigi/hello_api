5328ffe71c46c63bd2379f274ec4596aa980716a
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-04-14 11:15:24 +0300

    add unittest for admin edit book

--- tests/test_admin_actions.py
+++ tests/test_admin_actions.py
@@ -53,6 +53,47 @@ class AuthenticationTestCase(unittest.TestCase):
         self.assertEqual(result7.status_code, 400)
         self.assertEqual("Copies entered cannot be a negative number", (ast.literal_eval(result7.data))["message"])
         
+    def test_admin_edit_book(self):
+        '''test only admin can edit a book'''
+        # successful admin add book
+        result = self.client().post('/api/v1/books', headers=dict(Authorization="Bearer " + self.admin_token), content_type="application/json", data=json.dumps({"title":"The Davinci Code", "author":"Leonardo Davinci", "edition":"2nd", "copies":3}))
+        self.assertEqual(result.status_code, 201)
+        # regular user edit book
+        result2 = self.client().put('/api/v1/books/1', headers=dict(Authorization="Bearer " + self.user_token), content_type="application/json", data=json.dumps({"title":"Davinci's Demons"}))
+        self.assertEqual(result2.status_code, 403)
+        self.assertEqual("Admin privilege required", (ast.literal_eval(result2.data))["message"])
+        # successful admin edit book title
+        result3 = self.client().put('/api/v1/books/1', headers=dict(Authorization="Bearer " + self.admin_token), content_type="application/json", data=json.dumps({"title":"Davinci's Demons"}))
+        self.assertEqual(result3.status_code, 200)
+        self.assertEqual("Book 1 successfully edited", (ast.literal_eval(result3.data))["message"])
+        # empty fields
+        result4 = self.client().put('/api/v1/books/1', headers=dict(Authorization="Bearer " + self.admin_token), content_type="application/json", data=json.dumps({}))
+        self.assertEqual(result4.status_code, 400)
+        self.assertEqual("Enter valid data for edit", (ast.literal_eval(result4.data))["message"])
+        # empty strings
+        result5 = self.client().put('/api/v1/books/1', headers=dict(Authorization="Bearer " + self.admin_token), content_type="application/json", data=json.dumps({"title": ""}))
+        self.assertEqual(result5.status_code, 400)
+        self.assertEqual("all fields cannot be empty enter data to edit", (ast.literal_eval(result5.data))["message"])
+        # non-existent book
+        result6 = self.client().put('/api/v1/books/20', headers=dict(Authorization="Bearer " + self.admin_token), content_type="application/json", data=json.dumps({"title": ""}))
+        self.assertEqual(result6.status_code, 404)
+        self.assertEqual("book doesn't exist", (ast.literal_eval(result6.data))["message"])
+        # invalid data
+        result7 = self.client().put('/api/v1/books/1', headers=dict(Authorization="Bearer " + self.admin_token), content_type="application/json", data=json.dumps({"title": "   "}))
+        self.assertEqual(result7.status_code, 400)
+        self.assertEqual("Enter vaild data", (ast.literal_eval(result7.data))["message"])
+        # invalid data in copies
+        result8 = self.client().put('/api/v1/books/1', headers=dict(Authorization="Bearer " + self.admin_token), content_type="application/json", data=json.dumps({"copies": "four"}))
+        self.assertEqual(result8.status_code, 400)
+        self.assertEqual("Field copies has to be an integer and cannot be a negative number", (ast.literal_eval(result8.data))["message"])
+        # invalid status
+        result9 = self.client().put('/api/v1/books/1', headers=dict(Authorization="Bearer " + self.admin_token), content_type="application/json", data=json.dumps({"status": "gibberish"}))
+        self.assertEqual(result9.status_code, 400)
+        self.assertEqual("status has to be either available or unavailble", (ast.literal_eval(result9.data))["message"])
+        
+    def test_admin_delete_book(self):
+        '''test only admin can delete book'''
+
         
     def tearDown(self):
         with self.app.app_context():
