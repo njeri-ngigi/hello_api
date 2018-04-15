d1a59c8e8df6f4aebe1cf301c194e7386eb69916
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-04-16 02:35:02 +0300

    [Deploy] modify Procfile for heroku deployment

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,5 @@
 '''application/__init__.py'''
-from models import BookModel, UserModel, RevokedTokenModel, UserBooksModel
 from views import (Books, BooksBookId, Registration, RemoveUser, Login, Logout, ResetPassword,
                    ChangePassword, BorrowAndReturnBook, UserHistory)
-
+from models import BookModel, UserModel, RevokedTokenModel, UserBooksModel
 from app import create_app, db
-
